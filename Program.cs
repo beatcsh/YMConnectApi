504f372dac7ffab8d692339eae173ed8e1d0075a
beatcsh <josusssanher@gmail.com>
2025-08-01 10:06:18 -0600

    updating file from portable app

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@ using YMConnectApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5229); // lo ocupa docker
+});
+
 builder.Services.AddSingleton<RobotService>();
 builder.Services.AddHostedService<RobotBackgroundService>();
 
@@ -12,7 +17,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("http://localhost:3000")
               .AllowAnyHeader()
               .AllowAnyMethod()
-              .AllowCredentials(); // importante para credenciales
+              .AllowCredentials();
     });
 });
 
@@ -33,12 +38,11 @@ app.UseRouting();
 
 app.UseCors("AllowReactClient");
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.MapHub<RobotHub>("/robotHub");
 
 app.Run();
